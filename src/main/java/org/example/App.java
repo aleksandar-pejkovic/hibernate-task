6f5b80b69b4842dbd41b7ae4f27c2cd7e2c40c73
pejko <aleksandarpejkovic@hotmail.com>
2023-11-27 21:43:44 +0100

    fix imports

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,14 @@
 package org.example;
 
 import org.example.config.AppConfig;
+import org.example.config.HibernateConfig;
 import org.example.facade.GymFacade;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class App {
-    public static void main( String[] args ) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class,
+                HibernateConfig.class);
 
         GymFacade gymFacade = context.getBean(GymFacade.class);
         gymFacade.readTrainees();
--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,6 +1,12 @@
 package org.example.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
